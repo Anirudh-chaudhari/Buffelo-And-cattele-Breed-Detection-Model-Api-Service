0906909b5208595fce2050057a15662b6260478f
Anirudh-chaudhari <219702265+Anirudh-chaudhari@users.noreply.github.com>
2025-10-07 10:00:03 +0530

    Updated app.py for Runway deployment

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ app.add_middleware(
 )
 
 # Load YOLO model
-model = YOLO(r"D:\cow-breed-final\cow_breed_yolo11_FineTune.pt")
+model = YOLO(r"cow_breed_yolo11_FineTune.pt")
 
 # Load breed metadata JSON
-with open(r"D:\cow-breed-final\breed_info.json", "r", encoding="utf-8") as f:
+with open("breed_info.json", "r", encoding="utf-8") as f:
     breed_info = json.load(f)
 
 # Pydantic model for response
